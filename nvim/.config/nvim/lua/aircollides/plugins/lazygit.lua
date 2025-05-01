3249b3ca4838abea7f19414ea08f82e8d758a0a7
khanhthanhh9 <khanhthanhh9@gmail.com>
2025-05-02 03:11:52 +0700

    testing lazy git, pretty good

--- nvim/.config/nvim/lua/aircollides/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/aircollides/plugins/lazygit.lua
@@ -1,5 +1,7 @@
+-- nvim v0.8.0
 return {
 	"kdheepak/lazygit.nvim",
+	lazy = true,
 	cmd = {
 		"LazyGit",
 		"LazyGitConfig",
@@ -14,6 +16,6 @@ return {
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 	},
 }
